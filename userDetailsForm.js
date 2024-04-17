45629b183bc2e900b9f17a67bb6420282c7e4a35
ArpanKhuniya <arpan.khuniya@microlise.com>
2024-04-17 18:51:32 +0530

    Input fields should disapper once data is submitted and 12 fields added

--- userDetailsForm.js
+++ userDetailsForm.js
@@ -1,11 +1,12 @@
 function updateUserDetailsTable() {
-  // The preventDefault does not reload the page and the data wont be lost.
-  event.preventDefault();
-
+  
   // Values entered by user in the form
   let firstName = document.getElementById("firstName").value;
   let lastName = document.getElementById("lastName").value;
   let phoneNumber = document.getElementById("phoneNumber").value;
+
+  // To reset form input fields.
+  document.getElementById('userDetailsForm').reset();
 
   // Storing already present content in the table in userDetialsTableContent.
   let userDetialsTableContent =
@@ -18,6 +19,6 @@ function updateUserDetailsTable() {
           </tr>`;
 
   // Updating the table content.
-  document.getElementById("userDetailsTable").innerHTML =
-    userDetialsTableContent;
+  document.getElementById("userDetailsTable").innerHTML = userDetialsTableContent;
+  
 }
